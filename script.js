a81b578b54f06b702e3f799dfecfda3014a99c69
mecastrotapia <126998690+mecastrotapia@users.noreply.github.com>
2024-11-28 10:52:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let nombre = 'Juan';
+const edad = 25;
+console.log ( 'Hola, mi nombre es ${nombre} y tengo ${edad}años');
+
+let suma = 10 + 5
+console.log ('La suma de 10 +5 es ${suma}');
+
+if (edad = 18 ){
+    console.log ("Eres mayor de edad");
+
+}else{
+
+    console.log ("Eres menor de edad");
+}
