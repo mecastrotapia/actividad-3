572d8083274d912aa102f6547ed98f195d94e5ff
mecastrotapia <126998690+mecastrotapia@users.noreply.github.com>
2024-12-05 01:17:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-let nombre = "Juan";
-const edad = 25;
-console.log (`Hola, mi nombre es ${nombre} y tengo ${edad} años.`);
+//let nombre = "Juan";
+//const edad = 25;
+//console.log (`Hola, mi nombre es ${nombre} y tengo ${edad} años.`);
 
-let suma = 10 + 5
-console.log (`La suma de 10 +5 es ${suma}`);
+//let suma = 10 + 5
+//console.log (`La suma de 10 +5 es ${suma}`);
 
-if (edad >=18 ){
-    console.log ("Eres mayor de edad");
+//if (edad >=18 ){
+//    console.log ("Eres mayor de edad");
 
-}else{
+//}else{
 
-    console.log ("Eres menor de edad");
+//    console.log ("Eres menor de edad");
+//}
+
+ 
+let nombre = prompt ("ingresa tu nombre")
+if (nombre === "Mónica")
+    console.log (`Hola, ${nombre}`)
+else if (nombre === "Juan") {
+    console.log (`Bienvenido, ${nombre}`)
+}
+else if (nombre === "Carlos") {
+    console.log (`Hi, ${nombre}`)
+}
+else {
+    console.log (`No te conozco, bye`)
 }
 
-
+let suma = 50 + 25
+  console.log (`La suma de 50 + 25 es ${suma}`);
